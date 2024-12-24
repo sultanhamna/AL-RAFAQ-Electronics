b750eadb8923ad5dd79353addca6f24ab8acb4ad
hamna <hamnasultan220@gmail.com>
2024-12-24 17:24:37 +0500

    first commit

--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AL Rafaq Electronics</title>
+    <link rel="shortcut icon" href="{{ asset('assets/front/imges/title.ico') }}" type="image/x-icon">
     @include('admin.partial.style')
     @yield('style')
     <style>
